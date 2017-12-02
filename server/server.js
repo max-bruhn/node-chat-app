385adb3e750df1b5c20a224dfbfd538dc3a08649
Max Bruhn <mpcbruhn@gmail.com>
2017-12-02 20:17:15 +0700

    added connect and disc event / socket.io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 const path = require('path');
+
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');1
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+});
+
+
+
 app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.listen(port, () =>{
+server.listen(port, () =>{
     console.log('startet on port ' + port);
 } );
