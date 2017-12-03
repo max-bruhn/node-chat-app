8917e3ab7e934bb32888deef25cddef949f9a271
Max Bruhn <mpcbruhn@gmail.com>
2017-12-03 14:16:18 +0700

    emit newmsg on creamsg

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'andrew',
-        text: 'hey, this is andrew'
-    });
 });
 
 socket.on('disconnect', function () {
