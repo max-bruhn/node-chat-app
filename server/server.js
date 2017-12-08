b8ef4fd708b13ec6ddce2c90720cde69cd7cc61e
Max Bruhn <mpcbruhn@gmail.com>
2017-12-08 12:59:05 +0700

    added messaging in html

--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ io.on('connection', (socket) => {
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('this is form the server');
     });
 });
 
